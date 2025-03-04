1bcdfbecb711865c3982bcfc500488086e979f65
Huynhtienhieu1234 <huynhtienhieubasao@gmail.com>
2025-03-04 17:37:50 +0700

    Add project files.

---
+++ DangKiNgayLaoDong/Areas/Admin/Controllers/TrangNguoiQuanLyController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DangKiNgayLaoDong.Areas.Admin.Controllers
+{
+    public class TrangNguoiQuanLyController : Controller
+    {
+        // GET: Admin/TrangNguoiQuanLy
+  
+        public ActionResult Trangnguoiquanly()
+        {
+            return View();
+        }
+        public ActionResult Trangthongtinquanly()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ DangKiNgayLaoDong/Areas/Admin/Controllers/TrangthongtinquanlyController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DangKiNgayLaoDong.Areas.Admin.Controllers
+{
+    public class TrangthongtinquanlyController : Controller
+    {
+        // GET: Admin/Trangthongtinquanly
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
