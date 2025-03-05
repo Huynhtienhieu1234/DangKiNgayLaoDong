cc70040cc5de05703042f5b327b8050983c7ac15
Huynhtienhieu1234 <huynhtienhieubasao@gmail.com>
2025-03-05 13:50:42 +0700

    j

---
+++ DangKiNgayLaoDong/Areas/Admin/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DangKiNgayLaoDong.Areas.Admin.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Admin/Login
+        public ActionResult login()
+        {
+            return View();
+        }
+    }
+}
