51ae3d02371df492d23536cebdda2e0a47c20414
Huynhtienhieu1234 <huynhtienhieubasao@gmail.com>
2025-03-05 22:26:17 +0700

    jk

---
+++ DangKiNgayLaoDong/Areas/Admin/Controllers/XacNhanHoanThanhController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DangKiNgayLaoDong.Areas.Admin.Controllers
+{
+    public class XacNhanHoanThanhController : Controller
+    {
+        // GET: Admin/XacNhanHoanThanh
+        public ActionResult XacNhanHoanThanh()
+        {
+            return View();
+        }
+    }
+}
