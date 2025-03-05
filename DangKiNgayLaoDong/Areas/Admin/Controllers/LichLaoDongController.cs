aa32815cd26e03fdd4e06dc742180ee5c5993724
Huynhtienhieu1234 <huynhtienhieubasao@gmail.com>
2025-03-05 22:13:48 +0700

    new

---
+++ DangKiNgayLaoDong/Areas/Admin/Controllers/LichLaoDongController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DangKiNgayLaoDong.Areas.Admin.Controllers
+{
+    public class LichLaoDongController : Controller
+    {
+        // GET: Admin/LichLaoDong
+        public ActionResult LichLaoDong()
+        {
+            return View();
+        }
+    }
+}
