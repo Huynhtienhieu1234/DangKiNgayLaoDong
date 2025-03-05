efbaee8abc8327fe15c9907df4276dcf7008f7fd
Huynhtienhieu1234 <huynhtienhieubasao@gmail.com>
2025-03-05 20:40:01 +0700

    jkjk

---
+++ DangKiNgayLaoDong/Areas/Admin/Controllers/ThongBaoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DangKiNgayLaoDong.Areas.Admin.Controllers
+{
+    public class ThongBaoController : Controller
+    {
+        // GET: Admin/ThongBao
+        public ActionResult Thongbao()
+        {
+            return View();
+        }
+    }
+}
---
+++ DangKiNgayLaoDong/Areas/Admin/Controllers/ThongKeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DangKiNgayLaoDong.Areas.Admin.Controllers
+{
+    public class ThongKeController : Controller
+    {
+        // GET: Admin/ThongKe
+        public ActionResult ThongKe()
+        {
+            return View();
+        }
+    }
+}
