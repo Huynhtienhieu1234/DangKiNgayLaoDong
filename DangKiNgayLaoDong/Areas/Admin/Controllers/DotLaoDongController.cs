d9d67c49c3145e3a2563321a2bcb244f0719de8c
Huynhtienhieu1234 <huynhtienhieubasao@gmail.com>
2025-03-07 15:30:48 +0700

    s

---
+++ DangKiNgayLaoDong/Areas/Admin/Controllers/DotLaoDongController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DangKiNgayLaoDong.Areas.Admin.Controllers
+{
+    public class DotLaoDongController : Controller
+    {
+        // GET: Admin/DotLaoDong
+        public ActionResult Dotlaodong()
+        {
+            return View();
+        }
+    }
+}
